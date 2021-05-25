681944a133d86f576cc4ef9ed488e2606cf05668
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-05-25 15:34:02 -0300

    Resolve exercicio 1

---
+++ exercises/bloco_8/dia_1/exercises.js
@@ -0,0 +1,18 @@
+//1
+const newEmployees = (func) => {
+ const employees = {
+   id1: func('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+   id2: func('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+   id3: func('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+ }
+ return employees;
+};
+
+const newPerson = (name) => {
+ const person = {
+  nomeCompleto: name,
+  email: `${name.replace(' ','_').toLowerCase()}@trybe.com `,
+ }
+ return person;
+}
+console.log(newEmployees(newPerson));
